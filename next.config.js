e1b55c5469fa2967cfcce847014e28f1bedc7ea2
soheilycreations <soheilycreations@gmail.com>
2026-06-04 17:12:53 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Allow large PDF uploads
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
-    },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
